b1f84cfa0433b4962f3f7ade5e92facf479885e1
asmaa shaker <en.asmaashaker@gmail.com>
2024-08-31 00:07:50 +0300

    Add project files.

---
+++ Company.Data/Context/CompanyDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Company.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Data.Context
+{
+    public class CompanyDbContext:DbContext
+    {
+       
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           // optionsBuilder.UseSqlServer("");
+          
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department>  Departments { get; set; }
+    }
+}
